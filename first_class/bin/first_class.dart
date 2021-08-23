8ecf9c7a53e1d254f875f12ca012ba4a9487fa5d
antonio357 <carlosalbuquerque359@gmail.com>
2021-08-23 20:37:58 -0300

    variables, static type

--- first_class/bin/first_class.dart
+++ first_class/bin/first_class.dart
@@ -5,5 +5,11 @@ import 'dart:io';
 import 'package:first_class/first_class.dart' as first_class;
 
 void main(List<String> arguments) {
-  
+  // static type
+    print('with static type');
+    String name; int age; double salary;
+    print("digite seu nome:"); name = stdin.readLineSync();
+    print("digite sua age:"); age = int.parse(stdin.readLineSync());
+    print("digite seu salary:"); salary = double.parse(stdin.readLineSync());
+    print("Meu nome é $name, tenho $age anos e meu salário é $salary");
 }
