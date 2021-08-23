85aee94c5c311917afb2a9f49b2762fb21be384a
antonio357 <carlosalbuquerque359@gmail.com>
2021-08-23 20:39:05 -0300

    local scoping variables

--- first_class/bin/first_class.dart
+++ first_class/bin/first_class.dart
@@ -1,15 +1,18 @@
 // @dart=2.11 
 // turnning of null safety mode
+// using {} brakets do denife variables in local scopes
 
 import 'dart:io';
 import 'package:first_class/first_class.dart' as first_class;
 
 void main(List<String> arguments) {
-  // static type
+  {
+    // static type
     print('with static type');
     String name; int age; double salary;
     print("digite seu nome:"); name = stdin.readLineSync();
     print("digite sua age:"); age = int.parse(stdin.readLineSync());
     print("digite seu salary:"); salary = double.parse(stdin.readLineSync());
     print("Meu nome é $name, tenho $age anos e meu salário é $salary");
+  }
 }
