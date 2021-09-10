e4a10125c7e42307b36c82d5c3f40d1d9d35df3a
antonio357 <carlosalbuquerque359@gmail.com>
2021-09-09 21:43:12 -0300

    Statefull simple type counter

--- first_app/lib/app/pages/addForm.dart
+++ first_app/lib/app/pages/addForm.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:first_app/app/routes.dart';
 
 
-class AddForm extends StatelessWidget {
+class AddForm extends StatefulWidget {
   const AddForm({ Key? key }) : super(key: key);
+
+  @override
+  _AddFormState createState() => _AddFormState();
+}
+
+class _AddFormState extends State<AddForm> {
+  int _typeCounter = 0;
+
+  void _count() => setState(() => _typeCounter++);
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +22,14 @@ class AddForm extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text('You have typed times\n\n'),
+          Text('You have typed $_typeCounter times\n\n'),
           TextField(
             obscureText: true,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'type in here',
             ),
+            onChanged: (str) => _count(),
           )
         ]
       ),
